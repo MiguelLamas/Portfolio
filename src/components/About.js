365e5d81c1bad96aebaacfa8534c0868ce94634a
Miguel Lamas <lamasmigs@yahoo.co.uk>
2022-10-03 17:45:44 +0100

    changes to Home page hierarchy

--- src/components/About.js
+++ src/components/About.js
@@ -17,14 +17,14 @@ function About() {
                 <p>I have a real passion for collaborating, creativity and people. </p>
             </div>
             <div>
-                <p className="font-extralight my-1 mx-4"> 
+                <p className="font-light my-1 mx-4"> 
                 Before deciding to dive head first into a career in tech, I spent 15 years as a Photography Producer, managing and seeking out new business opportunities in advertising for commercial photographers. This helped me develop strong analytical, problem solving and people skills within a creative environment, which I really enjoy.
                 </p>
                 <br></br>
-                <p className="font-extralight my-1 mx-4">I can learn and adapt to new and complex challenges quickly, and aim to continue learning and evolving.
+                <p className="font-light my-1 mx-4">I can learn and adapt to new and complex challenges quickly, and aim to continue learning and evolving.
                 I'm keen to find my voice within the right tech role, where I can add value to a team, and continue to develop my skills working on products and experiences that bring value to peoples' lives.</p>
                 <br></br>
-                <p className="font-extralight my-1 mx-4">
+                <p className="font-light my-1 mx-4">
                 I'm passionate about music, and I love discovering new music and going to live shows. I'm also a keen football/sports fan, enjoy good food and wine, art/design, architecture, film and reportage photography.
                 </p>
             </div>

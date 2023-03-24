d3b036c389fa14bd58c3c4759544424c267aec05
Miguel Lamas <lamasmigs@yahoo.co.uk>
2023-03-24 16:38:40 +0000

    Added Rick & Morty Character Wiki to Projects Section of my Portfolio

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,6 @@
 import React from "react";
 import careFull from "../assets/careFull.png";
-import SoCSurvivalGuide from "../assets/SoCSurvivalGuide.png"
+import rickAndMorty from "../assets/rickAndMorty.png"
 import comingSoon3 from "../assets/comingSoon3.png"
 
 function Projects() {
@@ -10,8 +10,7 @@ function Projects() {
       <div className="projects-container">
         <div className="projects-div1">
           <p className="projects-p1">Projects</p>
-          <p className="projects-p2">Here are some of the projects I've been working on recently during the School of Code Bootcamp. 
-          I'll be adding some personal projects as I continue to learn, practice and build. Hover or click on each card to find out more.</p>
+          <p className="projects-p2">Here are some of the projects I've been working on recently. I'll be adding more projects as I continue to learn, build and develop. Hover or click on each card to find out more.</p>
         </div>
 
         {/* grid/card container */}
@@ -43,21 +42,21 @@ function Projects() {
 
            {/* grid/card item */}
           <div
-            style={{ backgroundImage: `url(${SoCSurvivalGuide})` }}
-            className="shadow-lg shadow-[#040c16] group container text-center flex justify-center items-center mx-auto content-div"
+            style={{ backgroundImage: `url(${rickAndMorty})` }}
+            className="shadow-lg shadow-[#040c16] group container flex justify-center items-center mx-auto content-div"
           >
             {/* Hover Effects */}
             <div className="opacity-0 group-hover:opacity-100">
             <span className="font-sans text-xl text-center font-bold text-[#000] tracking-wider">
-            SoC Survival Guide
+            Rick & Morty Character Wiki
               </span>
               <div className="pt-8 text-center">
-                <a href="http://socsurvivalguide.herokuapp.com/" target="_blank" rel="noreferrer">
+                <a href="https://rick-and-morty-character-wiki.netlify.app/" target="_blank" rel="noreferrer">
                   <button className="font-sans border border-black text-center px-4 py-3 m-2 bg-[#000] text-[#FFF] font-normal text-sm">
                     DEMO
                   </button>
                 </a>
-                <a href="https://github.com/MiguelLamas/week-project-back-end-soc-survival-guide" target="_blank" rel="noreferrer">
+                <a href="https://github.com/MiguelLamas/rick-and-morty-character-wiki" target="_blank" rel="noreferrer">
                   <button className="font-sans border border-black text-center px-4 py-3 m-2 bg-[#000] text-[#FFF] font-normal text-sm">
                     GITHUB
                   </button>
